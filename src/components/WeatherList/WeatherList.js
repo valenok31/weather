2831ac1ebdea12051eb1eba70bdedd15304cb578
Andrey <priv1984@yandex.ru>
2023-04-03 00:06:09 +0700

    - forecastday
    Working version 1.0

--- src/components/WeatherList/WeatherList.js
+++ src/components/WeatherList/WeatherList.js
@@ -1,20 +1,96 @@
 import React from "react";
 import {connect} from "react-redux";
+import {
+    handleCurrentIp,
+    handleCurrentWeather,
+    handleForecastWeather,
+    setSettings,
+    toggleIsLocationView
+} from "../../redux/weather_reducer";
+import s from "./WeatherList.module.css";
+import LocationOutput from "../LocationOutput/LocationOutput";
+import LocationSearch from "../LocationSearch/LocationSearch";
+import {HeaderContent} from "../HeaderContent";
+import Preloader from "../Preloader/Preloader";
 
 class WeatherList extends React.Component {
 
     componentDidMount() {
+        //this.props.handleCurrentIp();
+        this.props.handleCurrentWeather(this.props.getSettings);
+        this.props.handleForecastWeather(this.props.getSettings);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.getSettings !== prevProps.getSettings) {
+            this.props.handleCurrentWeather(this.props.getSettings);
+            this.props.handleForecastWeather(this.props.getSettings);
+        }
     }
 
     render() {
-        return <>s</>
+
+
+        if (!!this.props.getCurrentWeather.current) {
+            let getWeather = this.props.getCurrentWeather
+            let temp = getWeather.current.temp_c;
+            let currentLocation = getWeather.location
+            let currentWeather = getWeather.current
+            let nextDay = getWeather.forecast.forecastday
+            let windDegree = currentWeather.wind_degree;
+            let windKph = currentWeather.wind_kph;
+            //console.log(window)
+            let nextDayArr = nextDay.map((forecastday)=>{
+                // TODO: css!!
+              return  <div>
+                    <div>{forecastday.date}</div>
+                    <div>{forecastday.day.mintemp_c} ... {forecastday.day.maxtemp_c}</div>
+                    <div>{forecastday.day.maxwind_mph} m/s</div>
+                    <div>{forecastday.day.avghumidity} %</div>
+                </div>
+            })
+
+            return (<>
+                    <div>
+                        <div className={s.header__top}>
+                            {this.props.getIsLocationView ?
+                                <LocationOutput currentLocation={currentLocation}
+                                                toggleIsLocationView={this.props.toggleIsLocationView}/> :
+                                <LocationSearch setSettings={this.props.setSettings}
+                                                getSettings={this.props.getSettings}
+                                                currentLocation={currentLocation}
+                                                toggleIsLocationView={this.props.toggleIsLocationView}/>}
+                        </div>
+                        <div className={s.table}>
+                            {nextDayArr}
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </>
+            )
+        } else {
+            return <Preloader/>
+        }
     }
 }
 
 let mapStateToProps = (state) => {
-    return ({ })
+    return ({
+        getCurrentWeather: state.weather_reducer.currentWeather,
+        getForecastWeather: state.weather_reducer.forecastWeather,
+        getSettings: state.weather_reducer.settings,
+        getIsLoading: state.weather_reducer.isLoading,
+        getIsLocationView: state.weather_reducer.isLocationView,
+    })
 };
 
-let resultConnecting = connect(mapStateToProps, {})(WeatherList);
+let resultConnecting = connect(mapStateToProps, {
+    handleCurrentWeather,
+    handleForecastWeather,
+    setSettings,
+    handleCurrentIp,
+    toggleIsLocationView
+})(WeatherList);
 
 export default resultConnecting;
