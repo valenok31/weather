be5c3f46c3e7a3204b6c929cbfa3186afc91dfcf
Andrey <priv1984@yandex.ru>
2023-11-11 21:39:44 +0700

    - Setting.js -> hooks
    Working version 1.0

--- src/components/Setting/Setting.js
+++ src/components/Setting/Setting.js
@@ -13,92 +13,97 @@ import {Formik, Field, Form} from 'formik';
 // TODO: localization L10N
 import {l10n} from "../accessoryFunctions/localization"
 
-class Setting extends React.Component {
+//class Setting extends React.Component {
 
-    render() {
-        let getWeather = this.props.getSettings
-        let lang = getWeather.language;
-        localStorage.setItem('test', 1);
+//   render() {
 
-        console.log(localStorage.getItem('test'));
+function Setting(props) {
 
-        let formikOnSubmit = (values) => {
-            this.props.setSettingsWV(values);
-            this.props.setSettingsScaleTemperature(values);
-            this.props.setSettingsTheme(values);
-            this.props.setSettingsLanguage(values);
-            //console.log(values)
-        }
+    let getWeather = props.getSettings
+    let lang = getWeather.language;
+    localStorage.setItem('test', 1);
 
-        return (<>
-                <Formik
-                    initialValues={{
-                        windVisualization: getWeather.windVisualization,
-                        scaleTemperature: getWeather.scaleTemperature,
-                        theme: getWeather.theme,
-                        language: getWeather.language,
-                    }}
-                    onSubmit={formikOnSubmit}
-                >
-                    {({values}) => (
-                        <Form className={s.table} onClick={() => {
-                            this.props.toggleIsLocationView(true)
-                        }}>
-                            <div className={s.forecastday__box}>
-                                <div>
-                                    <span>{l10n['location'][lang]}:</span>
-                                    <span>{getWeather.location}</span>
-                                </div>
-                                <div>
-                                    <span>{l10n['language'][lang]}:</span>
-                                    <label>
-                                        <Field type="radio" name="language" value="en"/>
-                                        English
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="language" value="ru"/>
-                                        Русский
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="language" value="zh"/>
-                                        中文
-                                    </label>
-                                    <label>
-                                        <Field type="radio" name="language" value="es"/>
-                                        Español
-                                    </label>
-                                </div>
-                                {/*// TODO: theme*/}
-                                <div>
-                                    <span>{l10n['theme'][lang]}:</span>
-                                    <Field type="checkbox" name="theme"/>
-                                    {values.theme ? 'Светлая' : 'Тёмная'}
-                                </div>
-                                <div>
-                                    <label>
-                                        <span>{l10n['windVisualization'][lang]}:</span>
-                                        <Field type="checkbox" name="windVisualization"/>
-                                        {values.windVisualization ? l10n['on'][lang] : l10n['off'][lang]}
-                                    </label>
-                                </div>
-                                <div>
-                                    <label>
-                                        <span>{l10n['scaleTemperature'][lang]}:</span>
-                                        {/*                                        <span>{getWeather.scaleTemperature}</span>*/}
-                                        <Field type="checkbox" name="scaleTemperature"/>
-                                        {values.scaleTemperature ? '°C' : '°F'}
+    console.log(localStorage.getItem('test'));
 
-                                    </label>
-                                </div>
-                                <button type="submit">{l10n['submit'][lang]}</button>
-                            </div>
-                        </Form>
-                    )}
-                </Formik>
-            </>
-        )
+    let formikOnSubmit = (values) => {
+        props.setSettingsWV(values);
+        props.setSettingsScaleTemperature(values);
+        props.setSettingsTheme(values);
+        props.setSettingsLanguage(values);
+        //console.log(values)
+
     }
+
+    return (<>
+            <Formik
+                initialValues={{
+                    windVisualization: getWeather.windVisualization,
+                    scaleTemperature: getWeather.scaleTemperature,
+                    theme: getWeather.theme,
+                    language: getWeather.language,
+                }}
+                onSubmit={formikOnSubmit}
+            >
+                {({values}) => (
+                    <Form className={s.table} onClick={() => {
+                       props.toggleIsLocationView(true)
+                    }}>
+                        <div className={s.forecastday__box}>
+                            <div>
+                                <span>{l10n['location'][lang]}:</span>
+                                <span>{getWeather.location}</span>
+                            </div>
+                            <div>
+                                <span>{l10n['language'][lang]}:</span>
+                                <label>
+                                    <Field type="radio" name="language" value="en"/>
+                                    English
+                                </label>
+                                <label>
+                                    <Field type="radio" name="language" value="ru"/>
+                                    Русский
+                                </label>
+                                <label>
+                                    <Field type="radio" name="language" value="zh"/>
+                                    中文
+                                </label>
+                                <label>
+                                    <Field type="radio" name="language" value="es"/>
+                                    Español
+                                </label>
+                            </div>
+                            {/*// TODO: theme*/}
+                            <div>
+                                <span>{l10n['theme'][lang]}:</span>
+                                <Field type="checkbox" name="theme"/>
+                                {values.theme ? 'Светлая' : 'Тёмная'}
+                            </div>
+                            <div>
+                                <label>
+                                    <span>{l10n['windVisualization'][lang]}:</span>
+                                    <Field type="checkbox" name="windVisualization"/>
+                                    {values.windVisualization ? l10n['on'][lang] : l10n['off'][lang]}
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <span>{l10n['scaleTemperature'][lang]}:</span>
+                                    {/*                                        <span>{getWeather.scaleTemperature}</span>*/}
+                                    <Field type="checkbox" name="scaleTemperature"/>
+                                    {values.scaleTemperature ? '°C' : '°F'}
+
+                                </label>
+                            </div>
+                            <button type="submit">{l10n['submit'][lang]}</button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </>
+    )
 }
+
+//}
 
 
 let mapStateToProps = (state) => {
