51c255598189dc0ddc016a0f7630f10a6bf91db5
Andrey <93463435+valenok31@users.noreply.github.com>
2024-01-09 20:44:12 +0700

    Update auch_reducer.js
    1

--- src/redux/auch_reducer.js
+++ src/redux/auch_reducer.js
@@ -2,7 +2,6 @@ import {fetchIp, fetchWeather} from "../api/api_auch";
 
 const SET_CURRENT_WEATHER = 'SET_CURRENT_WEATHER';
 
-
 const initialState = {
     // TODO: language selection
     currentWeather: {},
@@ -15,14 +14,11 @@ const weather_reducer = (state = initialState, action) => {
                 ...state,
                 currentWeather: action.currentWeather
             }
-
         default:
             return state;
     }
 };
 
-
 export const setCurrentWeather = (currentWeather) => ({type: SET_CURRENT_WEATHER, currentWeather});
-
 
 export default weather_reducer;
